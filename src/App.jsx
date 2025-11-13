497544a82b3ad6fdb2b88e7eee0cf8db57adb9a8
Shiver3d <danielm8lopes@gmail.com>
2025-11-12 21:47:23 -0300

    primeiro commit.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Header from './components/Header/index.jsx'
+
+function App(){
+  return (
+    <>
+      <Header/>
+      <h1>passoia</h1>
+      <h2>testando react</h2>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,15 @@
+function Header() {
+  return (
+    <header>
+      <img src="https://m.media-amazon.com/images/I/71ClCABhjhL._AC_SY300_SX300_QL70_ML2_.jpg" alt="" />
+        <nav>
+            <ul>
+                <li><a href="#">Looks</a></li>
+                <li><a href="#">Lançamentos</a></li>
+                <li><a href="#">Novidades</a></li>
+            </ul>
+        </nav>
+    </header>
+  );
+}
+export default Header;
